45fa60bc07e1719fd1bdec2181d2fd293707d96a
namtransfr <pornphunwaree@gmail.com>
2026-01-18 16:54:47 +0700

    Delete portfolio directory

--- portfolio/src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-import { ThreeElements } from '@react-three/fiber'
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements extends ThreeElements {}
-  }
-}
